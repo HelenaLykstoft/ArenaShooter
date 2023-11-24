b7ad2d00dca2a2c5317e8215af776a552eb1cf41
thejamiegc <jcallan32@gmail.com>
2023-11-24 14:39:35 +0100

    Remakes player and Adds Gun prefab

---
+++ Assets/MovementPlayer.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementPlayer : MonoBehaviour
+{
+    private PlayerControls m_playerControls;
+    [SerializeField]private readonly float m_mouseSensitivity = 10.0f;
+
+    private readonly float m_upDownRange = 55.0f;
+    float rotY = 0;
+    float rotX = 0;
+    [SerializeField] private float speed = 5f;
+    CursorLockMode wantedMode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Awake()
+    {
+
+        m_playerControls = new PlayerControls();
+        m_playerControls.DefaultInput.onPress.performed += DoStuff;
+
+        wantedMode = CursorLockMode.Locked;
+
+    }
+
+    void FixedUpdate()
+    {
+        FPSMouseLook();
+        SetCursorState();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        // W movement
+        if (Keyboard.current != null && Keyboard.current.wKey.isPressed)
+        {
+            //Debug.Log("W was pressed!");
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        // A movement
+        if (Keyboard.current != null && Keyboard.current.aKey.isPressed)
+        {
+            //Debug.Log("A was pressed!");
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        // S movement
+        if (Keyboard.current != null && Keyboard.current.sKey.isPressed)
+        {
+            //Debug.Log("S was pressed!");
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+
+        // D movement
+        if (Keyboard.current != null && Keyboard.current.dKey.isPressed)
+        {
+            //Debug.Log("D was pressed!");
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        // escape locked cursor
+        if (Keyboard.current != null && Keyboard.current.escapeKey.isPressed)
+        {
+            wantedMode = CursorLockMode.None;
+        }
+        // left click to shoot
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            RaycastHit hit;
+
+            // ray from center of screen
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Debug.Log(hit.transform.name);
+                if (hit.transform.tag == "Enemy")
+                {
+                    //Destroy(hit.transform.gameObject);
+                    hit.transform.GetComponent<Renderer>().material.color = Color.black;
+                }
+            }
+        }
+    }
+
+    void OnEnable()
+    {
+        m_playerControls.DefaultInput.Enable();
+    }
+
+
+    void OnDisable()
+    {
+        m_playerControls.DefaultInput.Disable();
+    }
+
+    void DoStuff(InputAction.CallbackContext context)
+    {
+        Debug.Log("DoStuff called!");
+    }
+
+    private void FPSMouseLook()
+    {
+
+        rotY -= Input.GetAxis("Mouse Y") * m_mouseSensitivity;
+        rotX = Input.GetAxis("Mouse X") * m_mouseSensitivity;
+
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            transform.Rotate(0, rotX, 0);
+
+            rotY = Mathf.Clamp(rotY, -m_upDownRange, m_upDownRange);
+
+            Camera.main.transform.localRotation = Quaternion.Euler(rotY, 0, 0);
+        }
+    }
+
+    void SetCursorState()
+    {
+        Cursor.lockState = wantedMode;
+        // Hide cursor when locking
+        Cursor.visible = (CursorLockMode.Locked != wantedMode);
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,16 +10,25 @@ public class PlayerMovement : MonoBehaviour
     private GameObject mainCamera;
     [SerializeField] public float sensitivity = 2.0f; // Adjust the sensitivity of mouse movement
 
+    private readonly float m_upDownRange = 55.0f;
+    float rotY = 0;
+    float rotX = 0;
+    CursorLockMode wantedMode;
+
+    void FixedUpdate(){
+        CameraMovement();
+        SetCursorState();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        CameraMovement();
-        Debug.Log("Camera rotation: " + mainCamera.transform.rotation.eulerAngles);
-        
-    if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
-    {
-        RaycastHit hit;
+        //CameraMovement();
+        //Debug.Log("Camera rotation: " + mainCamera.transform.rotation.eulerAngles);
+
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            RaycastHit hit;
 
             // ray from center of screen
             Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
@@ -33,46 +42,52 @@ public class PlayerMovement : MonoBehaviour
                     hit.transform.GetComponent<Renderer>().material.color = Color.black;
                 }
             }
-    }
-    
+        }
 
-    // W movement
 
-    if (Keyboard.current != null && Keyboard.current.wKey.isPressed)
-    {
-    Debug.Log("W was pressed!");
-    transform.Translate(Vector3.forward * Time.deltaTime * speed);
-    }
+        // W movement
 
-    // A movement
-    if (Keyboard.current != null && Keyboard.current.aKey.isPressed)
-    {
-    Debug.Log("A was pressed!");
-    transform.Translate(Vector3.left * Time.deltaTime * speed);
-    }
+        if (Keyboard.current != null && Keyboard.current.wKey.isPressed)
+        {
+            Debug.Log("W was pressed!");
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
 
-    // S movement
-    if (Keyboard.current != null && Keyboard.current.sKey.isPressed)
-    {
-    Debug.Log("S was pressed!");
-    transform.Translate(Vector3.back * Time.deltaTime * speed);
-    }
+        // A movement
+        if (Keyboard.current != null && Keyboard.current.aKey.isPressed)
+        {
+            Debug.Log("A was pressed!");
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
 
-    // D movement
-    if (Keyboard.current != null && Keyboard.current.dKey.isPressed)
-    {
-    Debug.Log("D was pressed!");
-    transform.Translate(Vector3.right * Time.deltaTime * speed);
-    }
+        // S movement
+        if (Keyboard.current != null && Keyboard.current.sKey.isPressed)
+        {
+            Debug.Log("S was pressed!");
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
 
-    if (Pointer.current != null)
+        // D movement
+        if (Keyboard.current != null && Keyboard.current.dKey.isPressed)
+        {
+            Debug.Log("D was pressed!");
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+
+        if (Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            Debug.Log("esc was pressed!");
+            wantedMode = CursorLockMode.None;
+        }
+
+        if (Pointer.current != null)
         {
             if (Pointer.current.press.wasPressedThisFrame)
             {
                 var cam = Camera.main;
-                
-                Debug.Log("Pointer pressed at: "+ Pointer.current.position.ReadValue());
- 
+
+                Debug.Log("Pointer pressed at: " + Pointer.current.position.ReadValue());
+
                 var ray = cam.ScreenPointToRay(Pointer.current.position.ReadValue());
                 RaycastHit hit;
                 if (Physics.Raycast(ray, out hit))
@@ -81,7 +96,7 @@ public class PlayerMovement : MonoBehaviour
                 }
             }
         }
-        
+
     }
 
     void OnEnable()
@@ -101,29 +116,49 @@ public class PlayerMovement : MonoBehaviour
         //mainCamera = GetComponent<MainCamera>();
         m_playerControls = new PlayerControls();
         m_playerControls.DefaultInput.onPress.performed += DoStuff;
+        wantedMode = CursorLockMode.Locked;
     }
 
 
     void DoStuff(InputAction.CallbackContext context)
     {
-    Debug.Log("DoStuff called!");
+        Debug.Log("DoStuff called!");
     }
 
-    void CameraMovement(){
-        float mouseX = Input.GetAxis("Mouse X");
+    void CameraMovement()
+    {
+        /*float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
 
         // Rotate the camera based on mouse movement
-        transform.Rotate(Vector3.up, mouseX * sensitivity);
+        transform.Rotate(Vector3, mouseX * sensitivity);
         transform.Rotate(Vector3.left, mouseY * sensitivity);
 
         // Clamp the vertical rotation to prevent flipping
         float xRotation = transform.eulerAngles.x;
         if (xRotation > 180.0f)
             xRotation -= 360.0f;
-        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+            xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
 
         // Apply the clamped rotation
         transform.rotation = Quaternion.Euler(xRotation, transform.eulerAngles.y, 0.0f);
+*/
+        rotY -= Input.GetAxis("Mouse Y") * sensitivity;
+        rotX = Input.GetAxis("Mouse X") * sensitivity;
+
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            transform.Rotate(0, rotX, 0);
+
+            rotY = Mathf.Clamp(rotY, -m_upDownRange, m_upDownRange);
+            Camera.main.transform.localRotation = Quaternion.Euler(rotY, 0, 0);
+        }
+    }
+
+     void SetCursorState()
+    {
+        Cursor.lockState = wantedMode;
+        // Hide cursor when locking
+        Cursor.visible = (CursorLockMode.Locked != wantedMode);
     }
 }
